463d05a4d4a8ed80f93423792fb625cab17217fe
Carlos <carlosborjawebdev@gmail.com>
2025-03-04 17:18:23 +0100

    add placeholder layout

---
+++ app/sections/Header.tsx
@@ -0,0 +1,7 @@
+//This should take a series of parameters, then dynamically a header element that has those texts and moves there on click
+
+export default function Header() {
+  return (
+    <p>Cosas</p>
+  );
+}

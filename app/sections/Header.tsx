b93c79ec2c48bfa0ab6cdcc62ed54aacd3a3727f
Carlos <carlosborjawebdev@gmail.com>
2025-03-04 23:12:38 +0100

    Initial programmatic content of header

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -1,7 +1,14 @@
 //This should take a series of parameters, then dynamically a header element that has those texts and moves there on click
+import styles from "./Header.module.css"
+
+const test = ["One", "Two", "Three"];
 
 export default function Header() {
   return (
-    <p>Cosas</p>
+    <header className={styles.test}>
+      {test.map(element => 
+        <p>{element}</p>
+      )}
+    </header>
   );
 }

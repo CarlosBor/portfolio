fb06048033e438eb55a800a5c43f48cfdf3add5d
CarlosBor <carlosborjawebdev@gmail.com>
2025-03-10 01:16:25 +0100

    changes to be squished

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enables static export
-  basePath: "/portfolio", // Replace with your actual repo name (if applicable)
+  output: 'export', // Enables static export
+  basePath: '/portfolio', // Replace with your actual repo name (if applicable)
   images: { unoptimized: true }, // Disable Next.js image optimization (GitHub Pages doesn't support it)
 };
 
